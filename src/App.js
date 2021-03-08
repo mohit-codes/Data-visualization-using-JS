42cbddd6e983237efbbc94126bf8ff3fbd9d5b4b
mohit-codes <kushwahamohit000@gmail.com>
2021-03-08 19:02:39 +0530

    simple example with props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const options = {
+  series: [
+    {
+      name: "Profit",
+      data: [100, 200, 30, 100, 50, 30],
+    },
+  ],
+};
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {" "}
+      <HighchartsReact highcharts={Highcharts} options={options} />{" "}
+    </div>
+  );
 }
 
 export default App;
