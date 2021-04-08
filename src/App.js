73f84129cbc5f7c3ba88844f01f1894a490971ad
mohit-codes <kushwahamohit000@gmail.com>
2021-04-08 12:37:02 +0530

    csv parse using papaparse

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import "./App.css";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-
-const options = {
-  series: [
-    {
-      name: "Profit",
-      data: [100, 200, 30, 100, 50, 30],
-    },
-  ],
-};
+import Papa from "papaparse";
+import { useState } from "react";
 
 function App() {
+  const [dat, setDat] = useState([]);
+  const options = {
+    series: [
+      {
+        name: "Profit",
+        data: dat,
+      },
+    ],
+  };
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    Papa.parse(file, {
+      header: true,
+      dynamicTyping: true,
+      complete: function (results) {
+        console.log(results.data[0]);
+        setDat(results.data);
+      },
+    });
+  };
   return (
     <div className="App">
-      {" "}
+      <div>
+        {" "}
+        <input type="file" onChange={handleFileUpload}></input>{" "}
+      </div>
       <HighchartsReact highcharts={Highcharts} options={options} />{" "}
     </div>
   );
